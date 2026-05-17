57bd134ca28464c62b844754af418c50c6c9e2e9
WenderOk <wondergeorgy@yandex.ru>
2026-05-17 16:19:57 +0300

    refactor: some refactoring

--- src/tests/Setup.cpp
+++ src/tests/Setup.cpp
@@ -67,6 +67,30 @@ public:
     { return mName; }
 };
 
+TEST_EX("Test will run setup and teardown code", int)
+{
+    MereTDD::SetupAndTeardown<TempEntry> entry;
+
+    // If this was a project test, it might be called
+    // "Updating empty name throws". And the type thrown
+    // would not be an int.
+    updateTestEntryName(entry.id(), "");
+}
+
+TEST("Test will run multiple setup and teardown code")
+{
+    MereTDD::SetupAndTeardown<TempEntry> entry1;
+    MereTDD::SetupAndTeardown<TempEntry> entry2;
+
+    // If this was a project test, it might need
+    // more than one temporary entry. The TempEntry
+    // policy could either create multiple data records
+    // or it is easier to just have multiple instances
+    // that each create a single data entry.
+    updateTestEntryName(entry1.id(), "abc");
+    updateTestEntryName(entry2.id(), "def");
+}
+
 MereTDD::TestSuiteSetupAndTeardown<TempTable>
 gTable1("Test suite setup/teardown 1", "Suite 1");
 
@@ -86,14 +110,4 @@ TEST_SUITE_EX("Test part 2 of suite", "Suite 1", int)
     // If this was a project test, it could use
     // the table names from gTable1 and gTable2.
     throw 1;
-}
-
-TEST_EX("Test will run setup and teardown code", int)
-{
-    MereTDD::SetupAndTeardown<TempEntry> entry;
-    // If this was a project test, it might be called
-    // "Updating empty name throws". And the type thrown
-    // would not be an int.
-    updateTestEntryName(entry.id(), "");
-
 }
